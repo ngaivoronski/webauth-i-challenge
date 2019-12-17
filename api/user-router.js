5ff0834c06470c84fb54656eb409d0d44f965145
ngaivoronski <ngaivoronski@gmail.com>
2019-12-17 11:49:59 -0500

    added userlist only if logged in functionality using headers

--- api/user-router.js
+++ api/user-router.js
@@ -6,6 +6,35 @@ const router = express.Router();
 
 const bcrypt = require('bcryptjs');
 
+// Middleware
+
+function restricted(req, res, next) {
+    // we'll read the username and password from headers
+    // the client is responsible for setting those headers
+    const { username, password } = req.headers;
+  
+    // no point on querying the database if the headers are not present
+    if (username && password) {
+        Users.getUserBy({ username })
+            .first()
+            .then(user => {
+                if (user && bcrypt.compareSync(password, user.password)) {
+                    next();
+                } else {
+                    res.status(401).json({ message: 'Invalid Credentials' });
+                }
+            })
+            .catch(error => {
+                res.status(500).json({ message: 'Unexpected error' });
+            });
+    } else {
+        res.status(400).json({ message: 'No credentials provided' });
+    }
+} 
+
+
+// CRUD
+
 router.post('/register', (req, res) => {
     let user = req.body;
 
@@ -41,7 +70,7 @@ router.post('/login', (req,res)=>{
         });
 });
 
-router.get('/users', (req, res)=>{
+router.get('/users', restricted, (req, res)=>{
     Users.getUsers()
         .then(users => {
             res.status(200).json(users);
